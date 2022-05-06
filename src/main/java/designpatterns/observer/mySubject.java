cd4890a9b66b74dd46ca663f0ae5f906435c01c5
sb <s.88bin@gmail.com>
2022-05-06 20:20:46 +0900

    making mySubject interface

--- src/main/java/designpatterns/observer/mySubject.java
+++ src/main/java/designpatterns/observer/mySubject.java
@@ -1,5 +1,7 @@
 package designpatterns.observer;
 
 public interface mySubject {
-    
+    public void registerObserver(myObserver o);
+    public void removeObserver(myObserver o);
+    public void notifyObservers();
 }
